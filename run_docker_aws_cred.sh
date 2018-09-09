0e9c610f9ffb8afebc39b4cae3e26078fa4d1c85
Dana Swanstrom <danaswanstrom@gmail.com>
2018-09-09 11:22:31 -0500

    Removes comments

--- run_docker_aws_cred.sh
+++ run_docker_aws_cred.sh
@@ -4,10 +4,8 @@ AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker run \
-# Edit next two lines as needed
 -e S3_BUCKET='sample-vin-number-images' \
 -e S3_DIRECTORY='SampleImages' \
 -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-# This is the name of your docker image
 script1
