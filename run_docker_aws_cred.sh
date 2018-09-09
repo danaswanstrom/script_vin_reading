03c42db3fe887cede7f5a5cb33259da7e0b7bff9
Dana Swanstrom <danaswanstrom@gmail.com>
2018-09-09 11:17:21 -0500

    Updates the file to allow you to set parameters

--- run_docker_aws_cred.sh
+++ run_docker_aws_cred.sh
@@ -1,7 +1,14 @@
+# If you have configured your machine with aws configure the following
+# will allow you to access your credentials
 AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker run \
+# Edit the next two lines with your information
+-e S3_BUCKET = 'sample-vin-number-images'
+-e S3_DIRECTORY = 'SampleImages'
 -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+
+# This is the name of your docker image that you created
  script1
