05c162304dfcbdbbf60b73cda26c377bbb963c5f
Dana Swanstrom <danaswanstrom@gmail.com>
2018-09-10 06:53:19 -0500

    Adds barcode image indicator string to Environmental variable list

--- run_docker_aws_cred.sh
+++ run_docker_aws_cred.sh
@@ -1,18 +1,7 @@
 docker run \
 -e S3_BUCKET='Your Bucket' \
+-e BARCODE_IND_STRING='Place Unique String to Identify Barcodes Here' \
 -e S3_DIRECTORY='Directory in your bucket with jpgs' \
 -e AWS_ACCESS_KEY_ID='Your Access KEY' \
 -e AWS_SECRET_ACCESS_KEY='Your Secret key' \
 danaswanstrom/script_vin_reading
-#
-# Alternative below if you have use AWS configure on the machine 
-#
-# AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
-# AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
-#
-#docker run \
-#-e S3_BUCKET='sample-vin-number-images' \
-#-e S3_DIRECTORY='SampleImages' \
-#-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-#-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-#danaswanstrom/script_vin_reading
