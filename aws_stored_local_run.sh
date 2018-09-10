fd516b78fee78fbf7d01ed479d4b1082c22d8ef9
Dana Swanstrom <danaswanstrom@gmail.com>
2018-09-10 06:19:43 -0500

    Makes file more generic

--- aws_stored_local_run.sh
+++ aws_stored_local_run.sh
@@ -4,8 +4,8 @@ AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker run \
--e S3_BUCKET='sample-vin-number-images' \
--e S3_DIRECTORY='SampleImages' \
+-e S3_BUCKET='Your Bucket Name' \
+-e S3_DIRECTORY='Your Directory Name' \
 -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-danaswanstrom/vin_barcode_script_stretch:v1
+danaswanstrom/script_vin_reading
