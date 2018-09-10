75eae04e8ba9bc6751a1722386192d88660652b2
Dana Swanstrom <danaswanstrom@gmail.com>
2018-09-10 06:52:31 -0500

    Adds barcode image indicator string

--- aws_stored_local_run.sh
+++ aws_stored_local_run.sh
@@ -4,6 +4,7 @@ AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 docker run \
+-e BARCODE_IND_STRING='Place Unique String to Identify Barcodes Here' \
 -e S3_BUCKET='Your Bucket Name' \
 -e S3_DIRECTORY='Your Directory Name' \
 -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
